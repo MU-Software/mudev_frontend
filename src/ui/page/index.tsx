d53fe3d7c8ee2ef54e5d0ea42bb638ad9014eae2
MU-Software <musoftware@daum.net>
2023-12-24 00:01:58 +0900

    Add react-query and devtools to page index

--- src/ui/page/index.tsx
+++ src/ui/page/index.tsx
@@ -1,9 +1,10 @@
 import { mdiHome, mdiPlayCircleOutline, mdiPost } from '@mdi/js';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter } from 'react-router-dom';
 
-import { DummySidebar } from '@ui/component/bar/dummySidebar';
-import { Sidebar } from '@ui/component/bar/sidebar';
+import { DummySidebar, Sidebar } from '@ui/component/bar/sidebar';
 import { Topbar } from '@ui/component/bar/topbar';
 import { RouteDefinition, RouteDefinitionList } from '@ui/util/route_manager';
 
@@ -11,6 +12,8 @@ import { BlogMain } from '@page/blog';
 import { HomeMain } from '@page/home';
 import { PlayCoMain } from '@page/playco';
 import { NotFoundMain } from './internal/notfound';
+
+const queryClient = new QueryClient({ defaultOptions: { queries: { retry: 3, suspense: true } } })
 
 const ROUTE_DEFINITIONS: RouteDefinitionList = new RouteDefinitionList([
   new RouteDefinition('home', 'MUsoftware', '홈', '/', <HomeMain />, mdiHome),
@@ -20,10 +23,13 @@ const ROUTE_DEFINITIONS: RouteDefinitionList = new RouteDefinitionList([
 ]);
 
 export const Page: React.FC<{ className?: string; }> = ({ className }) => <div className={className}>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
   <BrowserRouter>
     <Topbar routeData={ROUTE_DEFINITIONS.topbarRoutes} />
     <Sidebar sidebarItems={ROUTE_DEFINITIONS.sidebarItems} />
     <DummySidebar />
     {ROUTE_DEFINITIONS.routes}
   </BrowserRouter>
+  </QueryClientProvider>
 </div>;
