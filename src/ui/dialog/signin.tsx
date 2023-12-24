f00b988f0f85c4706b1353d0b85090039a4353c4
MU-Software <musoftware@daum.net>
2023-12-25 02:37:00 +0900

    Add Signin Dialog

---
+++ src/ui/dialog/signin.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { css } from "@emotion/css";
+import { Form } from "react-bootstrap";
+
+import { PHDialog } from "@ui/component/layout/phDialog";
+
+type SignInDialogPropType = {
+    isOpen?: boolean;
+    closeDialog: () => void;
+};
+
+const SignInTextInputStyle = css({
+    width: "100%",
+});
+
+export const PHSignInDialog: React.FC<SignInDialogPropType> = props => {
+    const formRef = React.useRef<HTMLFormElement>(null);
+
+    const onSubmit = () => {
+        formRef.current?.reportValidity();
+        formRef.current?.requestSubmit();
+    }
+
+    return <PHDialog
+        header={<h4>로그인</h4>}
+        actions={[
+            { children: "취소", variant: "danger", onClick: props.closeDialog },
+            { children: "로그인", variant: "primary", onClick: onSubmit },
+        ]}
+        isCancelable
+        isOpen={props.isOpen}
+        closeDialog={props.closeDialog}
+    >
+        <Form ref={formRef}>
+            <Form.Group>
+                <Form.Label>이메일</Form.Label>
+                <Form.Control className={SignInTextInputStyle} type="text" placeholder="example@wesang.com" />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>비밀번호</Form.Label>
+                <Form.Control className={SignInTextInputStyle} type="password" placeholder="my_secret_password" />
+            </Form.Group>
+        </Form>
+    </PHDialog>;
+};
