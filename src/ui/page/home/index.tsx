8ab076703092b5d00bd0d9b209ac085a9899b91a
MU-Software <musoftware@daum.net>
2024-03-18 08:04:40 +0900

    공사 중 섹션 추가

--- src/ui/page/home/index.tsx
+++ src/ui/page/home/index.tsx
@@ -1,8 +1,10 @@
 import { PHPage } from '@local/ui/component/layout/phPage'
 import { HomeLogoSection } from './section/logo'
+import { UnderConstructionSection } from './section/logo/underConstruction'
 
 export const HomeMain = () => (
   <PHPage>
     <HomeLogoSection />
+    <UnderConstructionSection />
   </PHPage>
 )
---
+++ src/ui/page/home/section/logo/underConstruction.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { PHPageSection } from '@local/ui/component/layout/phPageSection'
+
+import './index.css'
+
+export const UnderConstructionSection: React.FC = () => (
+  <PHPageSection className="homeIndexSection">
+    <h3>현재 공사 중이에요!</h3>
+  </PHPageSection>
+)
