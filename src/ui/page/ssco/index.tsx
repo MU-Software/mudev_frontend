a005fbed496ad332feafdf69b95a73327a6d3ba0
MU-Software <musoftware@daum.net>
2024-03-18 08:12:57 +0900

    더미 SSCo 페이지 추가

---
+++ src/ui/page/ssco/index.tsx
@@ -0,0 +1,7 @@
+import { PHPageWithSection } from '@local/ui/component/layout/phPage'
+
+export const SSCoListPage = () => (
+  <PHPageWithSection>
+    <aside style={{ maxWidth: '50rem', width: '50%' }}>pass</aside>
+  </PHPageWithSection>
+)
