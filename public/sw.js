9821912cd8f3c5fe9159e4ca7498eab83a6d09f8
MU-Software <musoftware@daum.net>
2024-03-17 20:38:15 +0900

    sw.js 세미콜론 제거

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Minimal service worker for PWA support
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
+  event.respondWith(fetch(event.request))
+})
