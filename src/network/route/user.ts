6769664fc63983fee4ecbdb8a7bdd262170a1096
MU-Software <musoftware@daum.net>
2024-03-17 20:38:37 +0900

    사용자 요청 모듈에 로그인 기록 관련 함수 추가

--- src/network/route/user.ts
+++ src/network/route/user.ts
@@ -1,5 +1,5 @@
 import { fetchMU } from '@local/network/client'
-import { User } from '@local/network/schema/user.d'
+import { SignInHistory, User } from '@local/network/schema/user.d'
 
 export const fetchMyInfo = async () => {
   const response = await fetchMU({
@@ -10,4 +10,24 @@ export const fetchMyInfo = async () => {
     checkJSONParsable: true,
   })
   return JSON.parse(response.body) as User
+}
+
+export const fetchAllSignedInHistories = async () => {
+  const response = await fetchMU({
+    route: '/authn/signin-history/',
+    method: 'GET',
+    requireAuth: true,
+    checkStatus: true,
+    checkJSONParsable: true,
+  })
+  return JSON.parse(response.body) as SignInHistory[]
+}
+
+export const deleteSignInHistory = async (uuid: string) => {
+  await fetchMU({
+    route: `/authn/signin-history/${uuid}`,
+    method: 'DELETE',
+    requireAuth: true,
+    checkStatus: true,
+  })
 }
